f09df212e730ac459916f1c2c86b463fbc62f219
jcuratolo <jcuratolo@me.com>
2016-01-24 11:14:24 -0800

    init

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import store from './todoApp.js';
+import Header from './header.js';
+import Canvas from './Canvas.js';
+import Footer from './footer.js';
+
+var appContext;
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+    appContext = this;
+    store.subscribe(this.update.bind(this));
+  }
+
+  update() {
+    console.log('omg brittany im totally updating right now');
+    const storeState = store.getState();
+    this.setState(storeState);
+  }
+
+  addTodo(inputValue) {
+    store.dispatch({
+        type: 'ADD_TODO',
+        text: inputValue
+      });
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <Header onAddTodo={ this.addTodo.bind(this) }/>
+        <Canvas todos={ todos }/>
+        <Footer />
+      </div>
+    );
+  }
+};
