9f2653a30180e909ac38e35ce1c357aa9228b712
jcuratolo <jcuratolo@me.com>
2016-01-25 10:23:29 -0800

    change footer to functional component

--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-export default class Footer extends Component {
-  render() {
-    const { left, top } = this.props.imgPosition;
-    return (
-      <div>
-        <h3 style={{float: 'left'}}>Left: {left}</h3>
-        <h3 style={{float: 'left', marginLeft: 10}}>Top: {top}</h3>
-      </div>
-      )
-  }
-}
+const Footer = ({ imgPosition }) => {
+  const { left, top } = imgPosition;
+  return (
+    <div>
+      <h3 style={{float: 'left'}}>Left: {left}</h3>
+      <h3 style={{float: 'left', marginLeft: 10}}>Top: {top}</h3>
+    </div>
+  )
+};
+
+export default Footer;
